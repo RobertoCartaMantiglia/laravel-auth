92be374320bda4ba3c99269a16d79cdaaedb25b4
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-22 15:30:26 +0100

    add project in create

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -46,7 +46,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        return view('admin.projects.create', ["project" => new Project()]);
     }
 
     /**
