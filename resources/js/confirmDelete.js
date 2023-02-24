2b8277656d218d5bc3164af79e1443774d2fc707
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-24 15:26:55 +0100

    add comment

--- resources/js/confirmDelete.js
+++ resources/js/confirmDelete.js
@@ -1,5 +1,6 @@
 const confirmDeleteButton = document.querySelectorAll("form.delete_form");
 
+// per questa operazione fa utilizzato il querySelectorAll che prende tutti gli elemennti di cui necessito, a quel punto posso ciclarli in un foreach
 confirmDeleteButton.forEach((elementToDelete) => {
     elementToDelete.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -13,7 +14,7 @@ confirmDeleteButton.forEach((elementToDelete) => {
             confirmButtonText: 'Yes, delete it!'
         }).then((result) => {
             if (result.isConfirmed) {
-                this.submit()
+                this.submit() //bisogna richiamare l'evento se questo viene confermato per completare la delete
                 Swal.fire(
                     'Deleted!',
                     'Your file has been deleted.',
